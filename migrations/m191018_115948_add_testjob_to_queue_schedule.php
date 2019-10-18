8ecdd377f4b574cfb9c73ed02c073ceb52bf0c69
artmarkov <artmarkov@mail.ru>
2019-10-18 22:50:45 +0300

    refactor m191018_115948_add_testjob_to_queue_schedule

--- migrations/m191018_115948_add_testjob_to_queue_schedule.php
+++ migrations/m191018_115948_add_testjob_to_queue_schedule.php
@@ -6,7 +6,7 @@ class m191018_115948_add_testjob_to_queue_schedule extends Migration
 {
     public function up()
     {
-       $this->insert('{{%queue_schedule}}', ['id' => 1, 'title' => 'Тестовое задание', 'class' => 'artsoft\queue\jobs\TestJob', 'content' => 'Используется для тестирования приложения.', 'cron_expression' => '*/2 * * * *', 'priority' => 1024 ,'created_at' => time(), 'updated_at' => time(), 'created_by' => 1]);
+       $this->insert('{{%queue_schedule}}', ['id' => 1, 'title' => 'Тестовое задание', 'class' => 'artsoft\queue\jobs\TestJob', 'content' => 'Используется для тестирования приложения.', 'cron_expression' => '*/2 * * * *', 'priority' => 1024 ,'created_at' => time(), 'updated_at' => time(), 'created_by' => 1, 'updated_by' => 1]);
     }
 
     public function down()
