c46ad090da2a2956059e8fe55edab7cc0dbe5e15
Markov Artur <36476213+artmarkov@users.noreply.github.com>
2020-02-10 18:38:34 +0300

    refactor form

--- views/default/_form.php
+++ views/default/_form.php
@@ -12,88 +12,55 @@ use artsoft\models\User;
 
 <div class="queue-schedule-form">
 
-    <?php 
+    <?php
     $form = ActiveForm::begin([
-            'id' => 'queue-schedule-form',
-            'validateOnBlur' => false,
-        ])
+        'id' => 'queue-schedule-form',
+        'validateOnBlur' => false,
+    ])
     ?>
 
-    <div class="row">
-        <div class="col-md-8">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="row">
+                <div class="col-md-8">
+
                     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
-                    <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>                  
-                    
+                    <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
+
                     <?= $form->field($model, 'status')->dropDownList(QueueSchedule::getStatusList()) ?>
-                    
+
                     <?= $form->field($model, 'priority')->dropDownList(QueueSchedule::getPriorityList()) ?>
-                      
-                    <?= $form->field($model, 'class')->textInput(['disabled' => !User::hasPermission('editClassJob')]) ?> 
-                    
+
+                    <?= $form->field($model, 'class')->textInput(['disabled' => !User::hasPermission('editClassJob')]) ?>
+
                     <?= $form->field($model, 'cron_expression')->textInput() ?>
+                </div>
+                <div class="col-md-4">
+                    <?= \artsoft\queue\widgets\ExamplesCronWidget::widget(['model' => $model]); ?>
                 </div>
             </div>
         </div>
+        <div class="panel-footer">
+            <div class="form-group">
 
-        <div class="col-md-4">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="record-info">
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;"><?=  $model->attributeLabels()['id'] ?>: </label>
-                            <span><?=  $model->id ?></span>
-                        </div>
-                        <?php  if (!$model->isNewRecord): ?>
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;"><?=  $model->attributeLabels()['created_at'] ?>: </label>
-                            <span><?=  $model->createdDatetime ?></span>
-                        </div>
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;"><?=  $model->attributeLabels()['updated_at'] ?>: </label>
-                            <span><?=  $model->updatedDatetime ?></span>
-                        </div>
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;"><?=  $model->attributeLabels()['created_by'] ?>: </label>
-                            <span><?=  $model->createdBy->username ?></span>
-                        </div>
-                        <div class="form-group clearfix">
-                            <label class="control-label" style="float: left; padding-right: 5px;"><?=  $model->attributeLabels()['updated_by'] ?>: </label>
-                            <span><?=  $model->updatedBy->username ?></span>
-                        </div>
-                        <?php endif; ?>
-                        <div class="form-group">
-                            <?php  if ($model->isNewRecord): ?>
-                                <?= Html::submitButton(Yii::t('art', 'Create'), ['class' => 'btn btn-primary']) ?>
-                                <?= Html::a(Yii::t('art', 'Cancel'), ['/queue-schedule/default/index'], ['class' => 'btn btn-default']) ?>
-                            <?php  else: ?>
-                                <?= Html::submitButton(Yii::t('art', 'Save'), ['class' => 'btn btn-primary']) ?>
-                                <?= Html::a(Yii::t('art', 'Delete'),
-                                    ['/queue-schedule/default/delete', 'id' => $model->id], [
-                                    'class' => 'btn btn-danger',
-                                    'data' => [
-                                        'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
-                                        'method' => 'post',
-                                    ],
-                                ]) ?>
-                            <?php endif; ?>
-                        </div>
-                    </div>
-                </div>
+                <?= Html::a(Yii::t('art', 'Go to list'), ['/queue-schedule/default/index'], ['class' => 'btn btn-default']) ?>
+                <?= Html::submitButton(Yii::t('art', 'Save'), ['class' => 'btn btn-primary']) ?>
+                <?php if (!$model->isNewRecord): ?>
+                    <?= Html::a(Yii::t('art', 'Delete'),
+                        ['/queue-schedule/default/delete', 'id' => $model->id], [
+                            'class' => 'btn btn-danger',
+                            'data' => [
+                                'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                                'method' => 'post',
+                            ],
+                        ]) ?>
+                <?php endif; ?>
             </div>
-            <div class="panel panel-default">
-                <div class="panel-body">
-
-                    <?= \artsoft\queue\widgets\ExamplesCronWidget::widget(['model' => $model]); ?>
-
-                </div>
-            </div>
+            <?= \artsoft\widgets\InfoModel::widget(['model' => $model]); ?>
         </div>
     </div>
 
-    <?php  ActiveForm::end(); ?>
+    <?php ActiveForm::end(); ?>
 
 </div>
--- views/default/view.php
+++ views/default/view.php
@@ -15,25 +15,11 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <div class="row">
         <div class="col-sm-12">
-            <h3 class="page-title"><?=  Html::encode($this->title) ?></h3>            
+            <h3 class="page-title"><?= Html::encode($this->title) ?></h3>
         </div>
     </div>
     <div class="panel panel-default">
         <div class="panel-body">
-
-            <p>
-                <?= Html::a(Yii::t('art', 'Edit'), ['/queue-schedule/default/update', 'id' => $model->id], ['class' => 'btn btn-sm btn-primary'])?>
-                <?= Html::a(Yii::t('art', 'Delete'), ['/queue-schedule/default/delete', 'id' => $model->id], [
-                    'class' => 'btn btn-sm btn-danger',
-                    'data' => [
-                        'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
-                        'method' => 'post',
-                    ],
-                ]) ?>
-                <?= Html::a(Yii::t('art', 'Add New'), ['/queue-schedule/default/create'], ['class' => 'btn btn-sm btn-success pull-right']) ?>
-            </p>
-
-
             <?= DetailView::widget([
                 'model' => $model,
                 'attributes' => [
@@ -41,58 +27,70 @@ $this->params['breadcrumbs'][] = $this->title;
                     'title',
                     'content:ntext',
                     [
-                    'attribute' => 'status',
-                    'format' => 'raw',
-                    'value' => function (QueueSchedule $model) {
-                        return $model->getStatusList()[$model->status];
-                    },
+                        'attribute' => 'status',
+                        'format' => 'raw',
+                        'value' => function (QueueSchedule $model) {
+                            return $model->getStatusList()[$model->status];
+                        },
                     ],
                     [
-                    'attribute' => 'priority',
-                    'format' => 'raw',
-                    'value' => function (QueueSchedule $model) {
-                        return $model->getPriorityList()[$model->priority];
-                    },
+                        'attribute' => 'priority',
+                        'format' => 'raw',
+                        'value' => function (QueueSchedule $model) {
+                            return $model->getPriorityList()[$model->priority];
+                        },
                     ],
                     'cron_expression',
                     [
-                    'attribute' => 'nextDates',
-                    'value' => function (QueueSchedule $model) {
-                        $string = '';
+                        'attribute' => 'nextDates',
+                        'value' => function (QueueSchedule $model) {
+                            $string = '';
                             foreach ($model->getNextDates() as $date) {
                                 $string .= $date->format('d-m-Y H:i l') . PHP_EOL;
                                 $string .= '<br \>';
                             }
-                        return $string;
-                    },
-                    'format' => 'raw',
+                            return $string;
+                        },
+                        'format' => 'raw',
                     ],
                     [
-                    'attribute' => 'createdDatetime',                    
-                    'label' => $model->attributeLabels()['created_at'],
+                        'attribute' => 'createdDatetime',
+                        'label' => $model->attributeLabels()['created_at'],
                     ],
                     [
-                    'attribute' => 'updatedDatetime',
-                    'label' => $model->attributeLabels()['updated_at'],
-                    ],  
-                    [
-                    'attribute' => 'createdBy',
-                    'value' => function (QueueSchedule $model) {
-                        return $model->createdBy->username;
-                    },
-                    'label' => $model->attributeLabels()['created_by'],
+                        'attribute' => 'updatedDatetime',
+                        'label' => $model->attributeLabels()['updated_at'],
                     ],
                     [
-                    'attribute' => 'updatedBy',
-                    'value' => function (QueueSchedule $model) {
-                        return $model->updatedBy->username;
-                    },
-                    'label' => $model->attributeLabels()['updated_by'],
+                        'attribute' => 'createdBy',
+                        'value' => function (QueueSchedule $model) {
+                            return $model->createdBy->username;
+                        },
+                        'label' => $model->attributeLabels()['created_by'],
+                    ],
+                    [
+                        'attribute' => 'updatedBy',
+                        'value' => function (QueueSchedule $model) {
+                            return $model->updatedBy->username;
+                        },
+                        'label' => $model->attributeLabels()['updated_by'],
                     ],
                 ],
             ]);
-    ?>
-
+            ?>
+        </div>
+        <div class="panel-footer">
+            <div class="form-group">
+                <?= Html::a(Yii::t('art', 'Go to list'), ['/queue-schedule/default/index'], ['class' => 'btn btn-default']) ?>
+                <?= Html::a(Yii::t('art', 'Edit'), ['/queue-schedule/default/update', 'id' => $model->id], ['class' => 'btn btn-sm btn-primary']) ?>
+                <?= Html::a(Yii::t('art', 'Delete'), ['/queue-schedule/default/delete', 'id' => $model->id], [
+                    'class' => 'btn btn-sm btn-danger',
+                    'data' => [
+                        'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                        'method' => 'post',
+                    ],
+                ]) ?>
+            </div>
         </div>
     </div>
 
